d273e24c5863618c5d97de8fe778f5fc93f42023
Murtaza Dalal <mdalal@andrew.cmu.edu>
2021-04-28 12:08:24 -0700

    fixed embed pixel wrapper

--- rl/embed_pixel_wrapper.py
+++ rl/embed_pixel_wrapper.py
@@ -21,37 +21,30 @@ class EmbedPixelObservationWrapper(Wrapper):
 
 
     def render_obs(self, color_last=False):
-        with torch.no_grad():
-            raw_img = self.env.render(mode='rgb_array', height=self.source_img_width, width=self.source_img_width)
-            resized = skimage.transform.resize(raw_img, (self.img_width, self.img_width))
-            resized = resized.transpose([2, 0, 1])
-            self.imgs.pop(0)
-            self.imgs.append(resized)
-            processed_obs = self.encoder.encode_state(torch.from_numpy(np.array(self.imgs).reshape(-1, self.img_width, self.img_width)).float().cuda().unsqueeze(0))[0].cpu().detach().numpy()
-            return processed_obs
-
-    def render(self, *args, **kwargs):
         raw_img = self.env.render(mode='rgb_array', height=self.source_img_width, width=self.source_img_width)
         resized = skimage.transform.resize(raw_img, (self.img_width, self.img_width))
-        return resized * 255
+        if color_last: return resized
+        else: return resized.transpose([2, 0, 1])
+
+    def render(self, *args, **kwargs):
+        return self.render_obs(color_last=True) * 255
 
 
     def observation(self):
-        return np.concatenate(self.observations, axis=0)[-1]
-
+        obs = self.encoder.encode_state(torch.from_numpy(np.array(self.imgs).reshape(-1, self.img_width, self.img_width)).float().cuda().unsqueeze(0))[0].cpu().detach().numpy()[0]
+        return obs
 
     def step(self, action):
         obs, reward, done, info = self.env.step(action)
-        self.observations.pop(0)
-        self.observations.append(self.render_obs())
+        img = self.render_obs()
+        self.imgs.pop(0)
+        self.imgs.append(img)
         return self.observation(), reward, done, info
 
 
     def reset(self, **kwargs):
         self.env.reset(**kwargs)
-        self.imgs = [np.zeros([3, self.img_width, self.img_width]) for _ in range(self.stack)]
-        self.observations = [np.zeros([1, self.encoder.state_embed_size])
-                             for _ in range(self.stack - 1)] + [self.render_obs()]
+        self.imgs = [np.zeros([3, self.img_width, self.img_width]) for _ in range(self.stack - 1)] + [self.render_obs()]
         for _ in range(self.stack - 1):
             self.step(self.action_space.sample())
         self.env._elapsed_steps = 0
